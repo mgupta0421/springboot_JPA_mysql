2f01d3d26af527a2238974f93fa07b3ba7f29799
Mitali Gupta <mitaligupta726@gmail.com>
2024-08-06 15:56:22 -0400

    swagger integration

--- banking-app/src/main/java/net/javaguides/banking_app/controller/AccountController.java
+++ banking-app/src/main/java/net/javaguides/banking_app/controller/AccountController.java
@@ -1,6 +1,5 @@
 package net.javaguides.banking_app.controller;
 
-import org.hibernate.mapping.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import net.javaguides.banking_app.dto.AccountDto;
 import net.javaguides.banking_app.service.AccountService;
 
@@ -23,19 +25,28 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "Create new account")
+    @ApiResponse(responseCode = "200", description = "Account is created successfully")
     @PostMapping
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     } 
 
+    @Operation(summary = "Getting account by account id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Got account details by account id"),
+        @ApiResponse(responseCode = "400", description = "Account not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccount(@PathVariable Long id){
         AccountDto accountDto = accountService.getAccountById(id);
         return ResponseEntity.ok(accountDto);
     } 
 
+    @Operation(summary = "Deposite amount in the account")
+    @ApiResponse(responseCode = "200", description = "Amout added to the account")
     @PostMapping
-    public ResponseEntity<AccountDto> deposit(Long id, Map<String, Double> request){
+    public ResponseEntity<AccountDto> deposit(Long id, double request){
         AccountDto accountDto = accountService.getAccountById(id);
         return ResponseEntity.ok(accountDto);
     } 
